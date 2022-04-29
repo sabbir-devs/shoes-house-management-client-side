4f5880b9a6464445799ccd81493208c50a4acfa6
sabbir322 <sabbirhosan370@gmail.com>
2022-04-29 16:46:33 +0600

    environment variable set

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWSkyBrwu_yGpLHowKh3jecLNuEt4Ht0E",
-  authDomain: "warehouse-68b1a.firebaseapp.com",
-  projectId: "warehouse-68b1a",
-  storageBucket: "warehouse-68b1a.appspot.com",
-  messagingSenderId: "885481444266",
-  appId: "1:885481444266:web:8f589b5b5b0f5f3758d1d3"
+  apiKey:process.evn.REACT_APP_apiKey,
+  authDomain:process.evn.REACT_APP_authDomain,
+  projectId:process.evn.REACT_APP_projectId,
+  storageBucket:process.evn.REACT_APP_storageBucket,
+  messagingSenderId:process.evn.REACT_APP_messagingSenderId,
+  appId:process.evn.REACT_APP_appId,
 };
 
 // Initialize Firebase
