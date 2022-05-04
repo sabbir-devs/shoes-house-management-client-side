69d45e3a92644c8b72617692eb0b49273edbe680
sabbir322 <sabbirhosan370@gmail.com>
2022-05-04 14:29:30 +0600

    email password error set

--- src/pages/Home/Login/Login.js
+++ src/pages/Home/Login/Login.js
@@ -2,16 +2,47 @@ import React, { useState } from 'react';
 import './Login.css';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth } from '../../../firebase.init';
 
 const provider = new GoogleAuthProvider();
 const Login = () => {
-     const [showPass, setShowPass] = useState(false)
+     const [showPass, setShowPass] = useState(false);
+     const [email, setEmail] = useState({value:'', error:''});
+     const [password, setPassword] = useState({value:'', error:''})
     const navigate = useNavigate()
+
+    const emailInput = (emailInput) => {
+        if (/\S+@\S+\.\S+/.test(emailInput)) {
+            setEmail({ value: emailInput, error: "" });     
+        } else {
+            setEmail({ value: "", error: "Please Enter Valid Email" }); 
+        }
+
+    }
+    const passwordInput = (passwordInput) => {
+        if (passwordInput.length < 6) {
+            setPassword({ value: "", error: "Password Minimum of 6 Characters" });
+        } else {
+            setPassword({ value: passwordInput, error: "" });
+        }
+    }
+
     const halndleFormSubmit = event => {
         event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+    })
+    .catch((error) => {
+        console.log(error)
+    });
     }
 
     const handleGoogleSignIn = () => {
@@ -29,11 +60,13 @@ const Login = () => {
             <form onSubmit={halndleFormSubmit} className="form">
                 <h2>Login!!</h2>
                 <div className='inputs'>
-                    <input className='input' type="email" name="email" placeholder='Email' id="" />
+                    <input onBlur={(event) => emailInput(event.target.value)} className='input' type="email" name="email" placeholder='Email' id="" />
+                    {email.error && <><p className='error-text'><HiOutlineExclamationCircle style={{fontSize:'17px', marginRight:'5px'}}></HiOutlineExclamationCircle>plase enter valid email</p></>}
                 </div>
                 <div className='inputs'>
-                    <input className='input' type={showPass ? 'text' : 'password'} name="password" placeholder='Password' id="" />
+                    <input onBlur={(event) => passwordInput(event.target.value)} className='input' type={showPass ? 'text' : 'password'} name="password" placeholder='Password' id="" />
                     <label onClick={() => setShowPass(!showPass)} className='label' htmlFor="password">{showPass ? <BsFillEyeSlashFill></BsFillEyeSlashFill> : <BsFillEyeFill></BsFillEyeFill>}</label>
+                    {password.error && <><p className='error-text'><HiOutlineExclamationCircle style={{fontSize:'17px', marginRight:'5px'}}></HiOutlineExclamationCircle>Password Minimum of 6 Characters</p></>}
                 </div>
                 <div className='inputs'>
                     <p className='shadow'>forget password?</p>
--- src/pages/Home/Signup/Signup.js
+++ src/pages/Home/Signup/Signup.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './Signup.css';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from '../../../firebase.init';
 import { FcGoogle } from 'react-icons/fc';
 import { useNavigate } from 'react-router-dom';
@@ -9,10 +10,34 @@ import { useNavigate } from 'react-router-dom';
 
 const provider = new GoogleAuthProvider();
 const Signup = () => {
-    const [showPass, setShowPass] = useState(false)
-    const [showConPass, setShowConPass] = useState(false)
+    const [showPass, setShowPass] = useState(false);
+    const [showConPass, setShowConPass] = useState(false);
+    const [email, setEmail] = useState({value:'', error:''});
+    const [password, setPassword] = useState({value:'', error:''});
+    const [confirmPassword, setConfirmPassword] = useState({value:'', error:''});
     const navigate = useNavigate()
     
+    const emailInput = (emailInput) => {
+        if (/\S+@\S+\.\S+/.test(emailInput)) {
+            setEmail({ value: emailInput, error: "" });     
+        } else {
+            setEmail({ value: "", error: "Please Enter Valid Email" }); 
+        }
+    }
+    const passwordInput = (passwordInput) => {
+        if (passwordInput.length < 6) {
+            setPassword({ value: "", error: "Password Minimum of 6 Characters" });
+        } else {
+            setPassword({ value: passwordInput, error: "" });
+        }
+    }
+    const confirmPasswordInput = (confirmPasswordInput) => {
+        if (confirmPasswordInput !== password) {
+            setConfirmPassword({ value: confirmPasswordInput, error: "" });
+          } else {
+            setConfirmPassword({ value: "", error: "Password don't matched" });
+          }
+    }
 
     const halndleFormSubmit = event => {
         event.preventDefault();
@@ -20,6 +45,17 @@ const Signup = () => {
         const password = event.target.password.value;
         const confirmPassword = event.target.ConfirmPassword.value;
         console.log(email, password, confirmPassword)
+
+        createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    console.log(error)
+    // ..
+  });
     }
     const handleGoogleSignIn = () => {
         console.log('clicked')
@@ -37,15 +73,18 @@ const Signup = () => {
             <form onSubmit={halndleFormSubmit} className="form">
                 <h2>Register!!</h2>
                 <div className='inputs'>
-                    <input className='input' type="email" name="email" placeholder='Email' id="" />
+                    <input onBlur={(event) => emailInput(event.target.value)} className='input' type="email" name="email" placeholder='Email' id="" />
+                    {email.error && <><p className='error-text'><HiOutlineExclamationCircle style={{fontSize:'17px', marginRight:'5px'}}></HiOutlineExclamationCircle>plase enter valid email</p></>}
                 </div>
                 <div className='inputs'>
-                    <input className='input' type={showPass ? "text" : "password"} name="password" placeholder='Password' id="" />
+                    <input onBlur={(event) => passwordInput(event.target.value)} className='input' type={showPass ? "text" : "password"} name="password" placeholder='Password' id="" />
                     <label onClick={() => setShowPass(!showPass)} className='label' htmlFor="password">{showPass ? <BsFillEyeSlashFill></BsFillEyeSlashFill> : <BsFillEyeFill></BsFillEyeFill>}</label>
+                    {password.error && <><p className='error-text'><HiOutlineExclamationCircle style={{fontSize:'17px', marginRight:'5px'}}></HiOutlineExclamationCircle>{password.error}</p></>}
                 </div>
                 <div className='inputs'>
-                    <input className='input' type={showConPass ? "text" : "password"} name="ConfirmPassword" placeholder='Confirm Password' id="" />
+                    <input onBlur={(event) => confirmPasswordInput(event.target.value)} className='input' type={showConPass ? "text" : "password"} name="ConfirmPassword" placeholder='Confirm Password' id="" />
                     <label onClick={() => setShowConPass(!showConPass)} className='label' htmlFor="ConfirmPassword">{showConPass ? <BsFillEyeSlashFill></BsFillEyeSlashFill> : <BsFillEyeFill></BsFillEyeFill>}</label>
+                    {confirmPassword.error && <><p className='error-text'><HiOutlineExclamationCircle style={{fontSize:'17px', marginRight:'5px'}}></HiOutlineExclamationCircle>{confirmPassword.error}</p></>}
                 </div>
                 <div className='inputs'>
                     <p className='shadow'>forget password?</p>
