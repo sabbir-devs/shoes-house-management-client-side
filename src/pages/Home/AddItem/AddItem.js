0d66e48ab59013ec626d534f6485e3398c126d65
sabbir322 <sabbirhosan370@gmail.com>
2022-05-07 00:14:19 +0600

    add items added

--- src/pages/Home/AddItem/AddItem.js
+++ src/pages/Home/AddItem/AddItem.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import './AddItem.css'
+import './AddItem.css';
+
 
 const AddItem = () => {
+    const handleAddItems = event => {
+        event.preventDefault()
+    }
     return (
         <div className='add-item'>
+            <div className="item-add-form">
+                <form onSubmit={handleAddItems} className='form'>
+                    <input type="text" name="imgUrl" placeholder='Photo Url' id="" />
+                    <input type="text" name="name" placeholder='name' id="" />
+                    <input type="number" name="price" placeholder='Price' id="" />
+                    <input type="number" name="quantity" placeholder='Quantity' id="" />
+                    <input type="text" name="supplierName" placeholder='Supplier Name' id="" />
+                    <textarea name="description" placeholder='Shor Description' id=""></textarea>
+                </form>
+            </div>
             
         </div>
     );
--- src/pages/Home/Inventory/Inventory.js
+++ src/pages/Home/Inventory/Inventory.js
@@ -2,26 +2,27 @@ import React from 'react';
 import './Inventory.css';
 
 const shoes = [
-    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 5, suplier:'step hoker'},
-    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 25, suplier:'step hoker'},
-    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 45, suplier:'step hoker'},
-    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 35, suplier:'step hoker'},
-    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 25, suplier:'step hoker'},
-    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 35, suplier:'step hoker'}
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 5, suplier:'step hoker', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, doloribus? Quam autem eos ipsa unde iste atque perferendis eligendi itaque quasi voluptates. Placeat fuga quo exercitationem soluta ex sapiente labore.'},
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 25, suplier:'step hoker', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, doloribus? Quam autem eos ipsa unde iste atque perferendis eligendi itaque quasi voluptates. Placeat fuga quo exercitationem soluta ex sapiente labore.'},
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 45, suplier:'step hoker', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, doloribus? Quam autem eos ipsa unde iste atque perferendis eligendi itaque quasi voluptates. Placeat fuga quo exercitationem soluta ex sapiente labore.'},
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 35, suplier:'step hoker', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, doloribus? Quam autem eos ipsa unde iste atque perferendis eligendi itaque quasi voluptates. Placeat fuga quo exercitationem soluta ex sapiente labore.'},
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 25, suplier:'step hoker', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, doloribus? Quam autem eos ipsa unde iste atque perferendis eligendi itaque quasi voluptates. Placeat fuga quo exercitationem soluta ex sapiente labore.'},
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1aOK784vCCzvcN55BPSQYNaFEgDybKj2I8Yq-cHE2xuw0MIW0XOurQxDaSSHH__G2JM&usqp=CAU', name:'Nike Shoes', price:20, quantity: 35, suplier:'step hoker', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, doloribus? Quam autem eos ipsa unde iste atque perferendis eligendi itaque quasi voluptates. Placeat fuga quo exercitationem soluta ex sapiente labore.'}
 ]
 
 const Inventory = () => {
     return (
         <div className='inventory'>
-            <h1>inventory</h1>
+            <h1 style={{marginTop:'20px', textTransform:'uppercase'}}>inventory</h1>
             <div className="items">
             {shoes.map(shoe => (
                 <div className='item'>
                     <img src={shoe.img} alt="" />
                     <h3>{shoe.name}</h3>
-                    <h4>{shoe.price}</h4>
+                    <h4>${shoe.price}</h4>
                     <p>Quantity: ${shoe.quantity}</p>
                     <p>Suplier: {shoe.suplier}</p>
+                    <p title={shoe.description}>Description: {shoe.description.slice(0, 150)}</p>
                     <button>Update</button>
                 </div>
             ))}
