b1d287cc41d6a26875e7b08896868c07e16a0fb5
sabbir322 <sabbirhosan370@gmail.com>
2022-05-07 00:38:41 +0600

    add items form design done

--- src/pages/Home/AddItem/AddItem.js
+++ src/pages/Home/AddItem/AddItem.js
@@ -10,12 +10,13 @@ const AddItem = () => {
         <div className='add-item'>
             <div className="item-add-form">
                 <form onSubmit={handleAddItems} className='form'>
-                    <input type="text" name="imgUrl" placeholder='Photo Url' id="" />
-                    <input type="text" name="name" placeholder='name' id="" />
-                    <input type="number" name="price" placeholder='Price' id="" />
-                    <input type="number" name="quantity" placeholder='Quantity' id="" />
-                    <input type="text" name="supplierName" placeholder='Supplier Name' id="" />
-                    <textarea name="description" placeholder='Shor Description' id=""></textarea>
+                    <input className='adding-inputs' type="text" name="imgUrl" placeholder='Photo Url' id="" />
+                    <input className='adding-inputs' type="text" name="name" placeholder='name' id="" />
+                    <input className='adding-inputs' type="number" name="price" placeholder='Price' id="" />
+                    <input className='adding-inputs' type="number" name="quantity" placeholder='Quantity' id="" />
+                    <input className='adding-inputs' type="text" name="supplierName" placeholder='Supplier Name' id="" />
+                    <textarea className='adding-inputs' name="description" placeholder='Shor Description' id=""></textarea>
+                    <input className='add-btn' type="submit" value="Add" />
                 </form>
             </div>
             
